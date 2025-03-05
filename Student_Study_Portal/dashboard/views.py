0cb0e923c557ce1a1ae125d0c3d120cb9e95d98c
msSandhya23 <sandhyabehera33090@gmail.com>
2025-03-06 01:04:30 +0530

    Add Notes model, forms, and views for note-taking functionality

--- Student_Study_Portal/dashboard/views.py
+++ Student_Study_Portal/dashboard/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from . forms import Notes,NotesForm
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 def notes(request):
+    form = NotesForm()
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes':notes,'form':form}
     return render(request,'notes.html')
